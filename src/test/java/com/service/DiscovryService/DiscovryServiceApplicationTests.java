5f684f775f25b94025783afb34dade5d2f04b7ba
nmanikant <naidumanikanta1413@gmail.com>
2021-06-29 14:28:57 +0530

    first commit

---
+++ src/test/java/com/service/DiscovryService/DiscovryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.DiscovryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscovryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
